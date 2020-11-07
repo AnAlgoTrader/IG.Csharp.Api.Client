c082bec912dead80d3903fd44882973c0d6e1748
oneangrytrader <one.angry.trader@gmail.com>
2020-11-07 12:22:05 +0000

    no message

--- IG.Csharp.Api.Client/Helper/PositionHelper.cs
+++ IG.Csharp.Api.Client/Helper/PositionHelper.cs
@@ -1,4 +1,5 @@
 ﻿using IG.Csharp.Api.Client.Rest.Model;
+using IG.Csharp.Api.Client.Rest.Response;
 using System;
 
 namespace IG.Csharp.Api.Client.Helper
@@ -15,7 +16,13 @@ namespace IG.Csharp.Api.Client.Helper
             if (openPosition.Position.Direction == "BUY") return (openPosition.Market.Bid - openPosition.Position.Level) * openPosition.Position.Size;
             else return (openPosition.Position.Level - openPosition.Market.Offer) * openPosition.Position.Size;
         }
-
+        public static void CalculatePL(PositionsResponse positionsResponse)
+        {
+            positionsResponse.positions.ForEach(openPosition =>
+            {
+                openPosition.Position.ProfitAndLoss = CalculatePL(openPosition).Value;
+            });
+        }
         public static double? CalculatePL(OpenPosition openPosition, Streaming.Model.MarketData marketData)
         {
             if (openPosition.Position.Direction == "BUY") return (marketData.Bid - openPosition.Position.Level) * openPosition.Position.Size;
--- IG.Csharp.Api.Client/Rest/Response/PositionResponse.cs
+++ IG.Csharp.Api.Client/Rest/Response/PositionResponse.cs
@@ -1,13 +1,11 @@
 ﻿using IG.Csharp.Api.Client.Rest.Model;
+using System;
 using System.Collections.Generic;
 
 namespace IG.Csharp.Api.Client.Rest.Response
 {
     public class PositionsResponse
     {
-        ///<Summary>
-        ///List of positions
-        ///</Summary>
         public List<OpenPosition> positions { get; set; }
     }
 }
